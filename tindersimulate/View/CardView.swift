f05267d53863de5a55401ba5ee48d37e8537e6c8
Jannnn444 <yucianhuang@gmail.com>
2026-01-28 11:04:37 +0800

    Add label and config the label.attributedText

--- tindersimulate/View/CardView.swift
+++ tindersimulate/View/CardView.swift
@@ -12,13 +12,25 @@ class CardView: UIView {
     
     private let imageView: UIImageView = {
         let iv = UIImageView()
-        iv.contentMode = .scaleToFill
+        iv.contentMode = .scaleAspectFill
         iv.image = UIImage(named: "jane1")
         return iv
     }()
     
-    // MARK: - Lifecycle
+    private let infoLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 2
+        
+        let attributedText = NSMutableAttributedString(string: "Jane Doe", attributes: [.font: UIFont.systemFont(ofSize: 32, weight: .heavy), .foregroundColor: UIColor.white])
+        
+        attributedText.append(NSAttributedString(string: "  20", attributes: [.font: UIFont.systemFont(ofSize: 24), .foregroundColor: UIColor.white]))
+        
+        label.attributedText = attributedText
+        
+        return label
+    }()
     
+    // MARK: - Lifecycle
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = .systemPurple
@@ -28,6 +40,8 @@ class CardView: UIView {
         addSubview(imageView)
         imageView.fillSuperview()
         
+        addSubview(infoLabel)
+        infoLabel.anchor(left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingLeft: 16, paddingBottom: 16, paddingRight: 16)
     }
     
     required init?(coder: NSCoder) {
