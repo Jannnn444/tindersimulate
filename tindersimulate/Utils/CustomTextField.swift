244aa3fa9b0e34e9d82b50994faa8a80b5be4664
Jannnn444 <yucianhuang@gmail.com>
2026-02-02 16:29:11 +0800

    Create customized textfield

---
+++ tindersimulate/Utils/CustomTextField.swift
@@ -0,0 +1,31 @@
+//
+//  CustomTextField.swift
+//  tindersimulate
+//
+//  Created by Hualiteq International on 2026/2/2.
+//
+
+import UIKit
+
+class CustomTextField: UITextField {
+    
+    init(placeholder: String) {
+        super.init(frame: .zero)
+    
+    let spacer = UIView()
+    spacer.setDimensions(height: 50, width: 12)
+    leftView = spacer
+    leftViewMode = .always
+    
+    borderStyle = .none
+    textColor = .white
+    backgroundColor = UIColor(white: 1, alpha: 0.2)
+    heightAnchor.constraint(equalToConstant: 50).isActive = true
+    layer.cornerRadius = 5
+    attributedPlaceholder = NSAttributedString(string: placeholder, attributes: [.foregroundColor: UIColor(white: 1, alpha: 0.7)])
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
