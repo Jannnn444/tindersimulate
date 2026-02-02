6d3e30f8f6524bdd9989f422f723a56c12c1c95d
Jannnn444 <yucianhuang@gmail.com>
2026-02-02 15:36:25 +0800

    create textfield on LoginView

--- tindersimulate/ViewModel/Authentication/LoginController.swift
+++ tindersimulate/ViewModel/Authentication/LoginController.swift
@@ -18,6 +18,29 @@ class LoginController: UIViewController {
         return iv
     }()
     
+    private let emailTextField: UITextField = {
+        let tf = UITextField()
+        tf.borderStyle = .none
+        tf.textColor = .white
+        tf.backgroundColor = UIColor(white: 1, alpha: 0.2)
+        tf.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        tf.layer.cornerRadius = 5
+        tf.attributedPlaceholder = NSAttributedString(string: "Email", attributes: [.foregroundColor: UIColor(white: 1, alpha: 0.7)])
+        return tf
+    }()
+    
+    private let passwordTextField: UITextField = {
+        let tf = UITextField()
+        tf.borderStyle = .none
+        tf.textColor = .white
+        tf.backgroundColor = UIColor(white: 1, alpha: 0.2)
+        tf.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        tf.layer.cornerRadius = 5
+        tf.attributedPlaceholder = NSAttributedString(string: "Password", attributes: [.foregroundColor: UIColor(white: 1, alpha: 0.7)])
+        return tf
+    }()
+    
+    
     // MARK: - Lifecyele
     
     override func viewDidLoad() {
@@ -36,6 +59,14 @@ class LoginController: UIViewController {
         iconImageView.centerX(inView: view)
         iconImageView.setDimensions(height: 100, width: 100)
         iconImageView.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 32)
+        
+        let stack = UIStackView(arrangedSubviews: [emailTextField, passwordTextField])
+        stack.axis = .vertical
+        stack.spacing = 16
+        
+        view.addSubview(stack)
+        stack.anchor(top: iconImageView.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 24, paddingLeft: 32, paddingRight: 32)
+        UIStackView(backgroundColor: UIColor.blue) // color automatically add new stack?
     }
     
     func configureGradientLayerAtLoginPage() {
